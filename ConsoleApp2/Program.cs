8b5bbdfe7b635d84260a0922facb1447cb17a8a1
ShonenBAD <bader.messaoudi@hotmail.fr>
2024-07-11 13:37:52 +0200

    init repo with spire

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PdfLibrary;
+
+Console.WriteLine("Hello, World!");
+
+PdfProcessor p = new PdfProcessor();
---
+++ PdfLibrary/PdfProcessor.cs
@@ -0,0 +1,23 @@
+﻿using SkiaSharp;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using System;
+using System.IO;
+
+namespace PdfLibrary
+{
+    public class PdfProcessor
+    {
+        public PdfProcessor()
+        {
+            PdfDocument pdf = new PdfDocument();
+            pdf.LoadFromFile("testdoc.pdf");
+            Stream stream = pdf.SaveAsImage(0);
+            SKBitmap bitmap = SKBitmap.Decode(stream);
+            SKImage sKImage = SKImage.FromBitmap(bitmap);
+            FileStream fileStream = File.OpenWrite("testdoc.jpg");
+            sKImage.Encode(SKEncodedImageFormat.Jpeg, 100).SaveTo(fileStream);
+
+        }
+    }
+}
